ee0df4b25813d749ef620d031c94e17961881c23
北極狐 <91001011+actcfx@users.noreply.github.com>
2023-06-23 03:36:22 +0800

    more space, more comfortable ~

--- app/src/main/java/com/example/samoney/db.java
+++ app/src/main/java/com/example/samoney/db.java
@@ -19,7 +19,7 @@ public class db {
         String table_name = "t" + year + "y" + month + "m";
         create_table = "CREATE TABLE " + table_name +
                 "(_id INTEGER PRIMARY KEY, date INTEGER, time INTEGER, type TEXT, name TEXT, store TEXT, cost INTEGER)";
-        database = mCtx.openOrCreateDatabase(DATABASE_NAME, 0,null);
+        database = mCtx.openOrCreateDatabase(DATABASE_NAME, 0, null);
         try {
             database.execSQL(create_table);
         } catch (Exception exception) {
